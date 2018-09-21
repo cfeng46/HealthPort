0012d49af50cd44c0a1538852ca9c7eed58caf00
Tori Walker <twalker61@gatech.edu>
2018-09-21 11:32:52 -0400

    Commented out RecyclerView error line

--- app/src/main/java/com/example/cfeng/healthport/contacts.java
+++ app/src/main/java/com/example/cfeng/healthport/contacts.java
@@ -3,7 +3,7 @@ package com.example.cfeng.healthport;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
+//import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -23,7 +23,7 @@ public class contacts extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private DatabaseReference mDatabase;
     private TextView name;
-    private RecyclerView rcvListMessage;
+    //private RecyclerView rcvListMessage;
     //private ArrayList<String> mNames = new ArrayList<>();
     private ArrayList<Map<String, Object>> mNames= new ArrayList<Map<String, Object>>();
 
