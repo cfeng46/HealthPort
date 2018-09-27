9293b689ceb187d615844b74cc59218088d4d809
Emily <emilylanguyen122@gmail.com>
2018-09-27 17:32:01 -0400

    created change contact class

---
+++ app/src/main/java/com/example/cfeng/healthport/change_contact.java
@@ -0,0 +1,15 @@
+package com.example.cfeng.healthport;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.TextView;
+
+public class change_contact extends AppCompatActivity {
+}
