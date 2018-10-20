7a063ea03e2ace6fc869bb8168af29524e4ae402
Tori Walker <twalker61@gatech.edu>
2018-10-19 23:50:16 -0400

    Made fields on settings page populated

--- app/src/main/java/com/example/cfeng/healthport/settings.java
+++ app/src/main/java/com/example/cfeng/healthport/settings.java
@@ -7,19 +7,31 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class settings extends AppCompatActivity{
 
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        TextView emailText = findViewById(R.id.email_value);
+        TextView passText = findViewById(R.id.password_value);
         ImageView update_email = (ImageView) findViewById(R.id.change_email_icon);
         TextView update_email_text = (TextView) findViewById(R.id.change_email);
         ImageView update_pass = (ImageView) findViewById(R.id.change_password_icon);
         TextView update_pass_text = (TextView) findViewById(R.id.change_password);
         ImageView back = (ImageView) findViewById(R.id.backArrow);
+
+        emailText.setText(user.getEmail());
+        passText.setText("********");
 
         update_email.setOnClickListener(new View.OnClickListener() {
             @Override
