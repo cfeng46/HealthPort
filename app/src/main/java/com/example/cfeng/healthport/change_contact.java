9a804b1a34e25a68b9cc0d9746f62d83eaefb79b
jessicayu7 <qyu48@gatech.edu>
2018-11-09 12:52:59 -0500

    fix little bugs on contact

--- app/src/main/java/com/example/cfeng/healthport/change_contact.java
+++ app/src/main/java/com/example/cfeng/healthport/change_contact.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -14,11 +13,8 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 public class change_contact extends AppCompatActivity {
@@ -93,39 +89,14 @@ public class change_contact extends AppCompatActivity {
         mDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
         String user_id = mAuth.getCurrentUser().getUid();
         final DatabaseReference current_user_db = mDatabase.child(user_id).child("Contacts");
-        current_user_db.addListenerForSingleValueEvent(
-                new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        collectContactNames(dataSnapshot.getChildren());
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-                        //handle databaseError
-                    }
-                });
 
         if (updatedContactName.isEmpty() || updatedFaxNumber.isEmpty()) {
             Toast.makeText(change_contact.this,"Please type a name", Toast.LENGTH_SHORT).show();
         } else if (updatedFaxNumber.length() > 10) {
             Toast.makeText(change_contact.this,"Fax number invalid", Toast.LENGTH_SHORT).show();
 
-        } else if (updatedContactName.equals(sp_name)) {
-            current_contact_db.child("contact_"+Integer.valueOf(position)).child("Fax").setValue(updatedFaxNumber).addOnCompleteListener(new OnCompleteListener<Void>() {
-                @Override
-                public void onComplete(@NonNull Task<Void> task) {
-                    if(task.isSuccessful()) {
-                        Toast.makeText(change_contact.this, "Contact Updated", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(change_contact.this, contact.class);
-                        intent.putExtra("contact_name", position+","+sp_name+":"+updatedFaxNumber);
-                        startActivity(intent);
-                    }
-                }
-            });
-
         } else {
-            current_user_db.child("contact_"+Integer.valueOf(position)).removeValue();
+            //current_user_db.child("contact_"+Integer.valueOf(position)).removeValue();
             current_user_db.child("contact_"+Integer.valueOf(position)).child("Name").setValue(updatedContactName);
             current_user_db.child("contact_"+Integer.valueOf(position)).child("Fax").setValue(updatedFaxNumber).addOnCompleteListener(new OnCompleteListener<Void>() {
                 @Override
@@ -140,27 +111,11 @@ public class change_contact extends AppCompatActivity {
 
             });
         }
-
-
-    }
-
-    private void collectContactNames(Iterable<DataSnapshot> contacts) {
-
-        for (DataSnapshot singleContact:contacts){
-            int i = 0;
-            ArrayList<String> nest_list = new ArrayList<>();
-            for (DataSnapshot b: singleContact.getChildren()) {
-                nest_list.set(i, (String.valueOf(b.getValue())));
-                i++;
-            }
-            contactsList.add(nest_list);
-        }
-
     }
     private ArrayList<String> getIncomingIntent() {
         if (getIntent().hasExtra("dbContact")) {
             String contactName = getIntent().getStringExtra("dbContact");
-            Log.e("djksjkjrkjksjkfjks",contactName);
+            //Log.e("djksjkjrkjksjkfjks",contactName);
             String[] items_1 = contactName.split(",");
             for(String temp:items_1) {
                 if(position == null) {
@@ -177,12 +132,6 @@ public class change_contact extends AppCompatActivity {
                     sp_fax = temp;
                 }
             }
-            //TextView nameText = findViewById(R.id.name_value);
-           // nameText.setText(sp_name);
-           // String contact_fax = sp_fax;
-            //Log.e("denug", contact_fax);
-           // TextView fax_text = findViewById(R.id.fax_number_value);
-           // fax_text.setText(contact_fax);
             ArrayList<String> a = new ArrayList<>();
             a.add(position);
             a.add(sp_name);
